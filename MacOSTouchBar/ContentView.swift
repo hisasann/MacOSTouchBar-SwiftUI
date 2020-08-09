171c6a514c3b660a41397a4c78d738bc4bc8189e
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-08-09 16:21:51 +0900

    Initial Commit

---
+++ MacOSTouchBar/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacOSTouchBar
+//
+//  Created by 久松由幸 on 2020/08/09.
+//  Copyright © 2020 久松由幸. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
